fe626c53d8a3be6af41348a08994fafd5e59a41b
DESKTOP-EQORGJC\void <110916peter>
2021-12-30 15:42:54 +0800

    add select file limit

--- usart/ASA_HMI_Data_Agent_v2.Designer.cs
+++ usart/ASA_HMI_Data_Agent_v2.Designer.cs
@@ -646,6 +646,7 @@
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "IHex檔案|*.hex";
             // 
             // ASA_HMI_Data_Agent_v2
             // 
